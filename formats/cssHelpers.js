41b1ce573d6a1a67f70e3ba2dc6c17b16e990f99
Danny Banks <djb@amazon.com>
2019-07-16 10:51:49 -0700

    https://www.youtube.com/watch?v=dQw4w9WgXcQ

--- formats/cssHelpers.js
+++ formats/cssHelpers.js
@@ -1,5 +1,4 @@
 module.exports = function(dictionary, config) {
-  // Dictionary is filtered
   return dictionary.allProperties.map(function(prop) {
     return `.${prop.name} { ${prop.cssProp}: ${prop.value}; }`
   }).join('\n');
